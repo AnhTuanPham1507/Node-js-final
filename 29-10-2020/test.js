80e8c4f6100dcc26ee1426bfaf227abec8ca9d0c
Pham Anh Tuan <phamanhtuan9a531@gmail.com>
2020-11-03 15:10:54 +0700

    upload push request

--- 29-10-2020/test.js
+++ 29-10-2020/test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const expressHandleBars = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 function getLocations(CityName,callBack){
 	request(`https://www.metaweather.com/api/location/search/?query=${CityName}`,{json:true},(err,res,body)=>{
 			if(body.length===0)
